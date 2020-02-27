345824de669700f9a51299ee1fc3265516f858bb
Nikhil Pardasani <nikhil_pardasani@yahoo.com>
2020-02-27 19:16:19 +0530

    Initial Commit

---
+++ SplashScreenApp.Android/MainActivity.cs
@@ -0,0 +1,83 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using Android.Util;
+using Android.Views;
+using Plugin.CurrentActivity;
+using Xamarin.Forms.Platform.Android;
+
+namespace SplashScreenApp.Droid
+{
+    [Activity(Label = "Anki+", Theme = "@style/MainTheme", MainLauncher = false)]
+    public class MainActivity : FormsAppCompatActivity
+    {
+        public static MainActivity Intance;
+
+        public static DisplayMetrics displayMetrics;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            //Window.AddFlags(WindowManagerFlags.Fullscreen);
+
+            base.OnCreate(savedInstanceState);
+            Intance = this;
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Rg.Plugins.Popup.Popup.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Start();
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
+            
+            displayMetrics = new DisplayMetrics();
+            WindowManager.DefaultDisplay.GetRealMetrics(displayMetrics);
+
+            //==================================== Code to hide the bottom 3 buttons on Android.
+            int uiOptions = (int)Window.DecorView.SystemUiVisibility;
+
+            uiOptions |= (int)SystemUiFlags.LowProfile;
+            uiOptions |= (int)SystemUiFlags.Fullscreen;
+            uiOptions |= (int)SystemUiFlags.HideNavigation;
+            uiOptions |= (int)SystemUiFlags.ImmersiveSticky;
+
+            Window.DecorView.SystemUiVisibility = (StatusBarVisibility)uiOptions;
+            //====================================
+            LoadApplication(new App());
+
+            //if (Window != null) Window.SetStatusBarColor(Android.Graphics.Color.Transparent);
+            if (isPhone(this)) RequestedOrientation = ScreenOrientation.Portrait;
+
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            int uiOptions = (int)Window.DecorView.SystemUiVisibility;
+
+            uiOptions |= (int)SystemUiFlags.LowProfile;
+            uiOptions |= (int)SystemUiFlags.Fullscreen;
+            uiOptions |= (int)SystemUiFlags.HideNavigation;
+            uiOptions |= (int)SystemUiFlags.ImmersiveSticky;
+
+            Window.DecorView.SystemUiVisibility = (StatusBarVisibility)uiOptions;
+        }
+
+        public void Start()
+        {
+        }
+
+        public static bool isPhone(Context context)
+        {
+            return true;
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+
+    }
+}
---
+++ SplashScreenApp.Android/SplashActivity.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+
+namespace SplashScreenApp.Droid
+{
+    [Activity(Label = "Anki+", Theme = "@style/LaunchTheme", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+
+            //==================================== Code to hide the bottom 3 buttons on Android.
+            int uiOptions = (int)Window.DecorView.SystemUiVisibility;
+            uiOptions |= (int)SystemUiFlags.LowProfile;
+            uiOptions |= (int)SystemUiFlags.Fullscreen;
+            uiOptions |= (int)SystemUiFlags.HideNavigation;
+            uiOptions |= (int)SystemUiFlags.ImmersiveSticky;
+            Window.DecorView.SystemUiVisibility = (StatusBarVisibility)uiOptions;
+            //===================================
+
+            base.SetTheme(Resource.Style.MainTheme);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            //==================================== Code to hide the bottom 3 buttons on Android.
+            int uiOptions = (int)Window.DecorView.SystemUiVisibility;
+            uiOptions |= (int)SystemUiFlags.LowProfile;
+            uiOptions |= (int)SystemUiFlags.Fullscreen;
+            uiOptions |= (int)SystemUiFlags.HideNavigation;
+            uiOptions |= (int)SystemUiFlags.ImmersiveSticky;
+            Window.DecorView.SystemUiVisibility = (StatusBarVisibility)uiOptions;
+            //===================================
+
+            System.Threading.Tasks.Task startupWork = new System.Threading.Tasks.Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+
+        // Simulates background work that happens behind the splash screen
+        async void SimulateStartup()
+        {
+            await System.Threading.Tasks.Task.Delay(1000); // Simulate a bit of startup work.
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public override void OnBackPressed() { }
+    }
+}
